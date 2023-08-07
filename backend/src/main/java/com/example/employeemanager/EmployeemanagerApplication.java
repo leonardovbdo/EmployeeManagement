ca12c932b006e3aa6d5035cbd9d33f84baae0bc3
leonardovbdo <odranoelrubi@gmail.com>
2023-08-07 14:34:40 -0300

    Correções

--- backend/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
+++ backend/src/main/java/com/example/employeemanager/EmployeemanagerApplication.java
@@ -1,5 +1,8 @@
 package com.example.employeemanager;
 
+import com.example.employeemanager.model.Employee;
+import com.example.employeemanager.repository.EmployeeRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +17,34 @@ public class EmployeemanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeemanagerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(EmployeeRepository employeeRepository) {
+		return args -> {
+			employeeRepository.save(
+					new Employee(
+							null,
+							"Leonardo Vilasboas de Oliveira",
+							"leonardo@gmail.com",
+							"7799990000",
+							"Developer",
+							"https://avatars.githubusercontent.com/u/113204702?v=4",
+							"0001"
+					)
+			);
+			employeeRepository.save(
+					new Employee(
+							null,
+							"Matheus",
+							"Matheus@gmail.com",
+							"7799990001",
+							"CEO",
+							"https://img.freepik.com/premium-photo/happy-black-man-business-manager-ceo-working-modern-office-desk-as-financial-investor-stock-market-trader-corporate-worker-nigeria-portrait-smile-african-executive-leader-boss_590464-119404.jpg?w=740",
+							"0002"
+					)
+			);
+		};
 	}
 
 	@Bean
